4fa12a7433fb715daf987f52fdf6001394221fd9
Alcides Lemos <alcideslm1@gmail.com>
2019-05-01 11:14:41 -0300

    Adicionado template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1><i class="fa fa-check"></i>Lista de tarefas</h1>
+    <div id="todo-list">
+      <span class="user">Alcides Lemos Meira</span>
+      <div id="add-todo">
+        <i class="fa fa-plus"></i>
+        Adicionar usuario
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
